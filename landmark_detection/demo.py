be65f0dad446f53f44980375bec1b13480a3301e
Nikita Zakrevsky <m.zakreuski@sam-solutions.com>
2021-05-17 14:28:56 +0300

    saving result

--- landmark_detection/demo.py
+++ landmark_detection/demo.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--image', type=str, help='The evaluation image path.')
     parser.add_argument('--face', nargs='+', type=float, help='The coordinate [x1,x2,y1,y2] of a face')
+    parser.add_argument('--save_path', type=str, help='The path to save the visualization results')
 
     args = parser.parse_args()
 
@@ -17,3 +18,8 @@ if __name__ == "__main__":
     landmark_detector = LandmarkDetector('../checkpoint/model.pth')
     preprocessed_data = landmark_detector.preprocess_image(image, box)
     landmarks = landmark_detector.predict(preprocessed_data)
+
+    for point in landmarks['landmarks']:
+        face_image_orig = cv2.circle(image, (int(point[0]), int(point[1])), 3, (255, 0, 0), 1)
+
+    cv2.imwrite(args.save_path, image)

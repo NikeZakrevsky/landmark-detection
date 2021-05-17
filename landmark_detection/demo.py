a7efd1c453f5cf5a6bcf5e21fff026da32458e29
Nikita Zakrevsky <m.zakreuski@sam-solutions.com>
2021-05-17 14:22:34 +0300

    parser

--- landmark_detection/demo.py
+++ landmark_detection/demo.py
@@ -1,9 +1,18 @@
+import argparse
+
 from landmark_detection.landmark_detector import LandmarkDetector
 import cv2
 
 if __name__ == "__main__":
-    image = cv2.imread('Image.png')
-    box = [432, 575, 819, 971]
+    parser = argparse.ArgumentParser(description='Evaluate a single image by the trained model',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--image', type=str, help='The evaluation image path.')
+    parser.add_argument('--face', nargs='+', type=float, help='The coordinate [x1,x2,y1,y2] of a face')
+
+    args = parser.parse_args()
+
+    image = cv2.imread(args.image)
+    box = args.face
 
     landmark_detector = LandmarkDetector('../checkpoint/model.pth')
     preprocessed_data = landmark_detector.preprocess_image(image, box)
---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='landmark-detection',
+      version='1.0',
+      description='Landmark detection',
+      author='Nikita',
+      author_email='nick@python.net',
+     )
